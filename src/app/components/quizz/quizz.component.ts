a7a3862d27b03210c94894d3ec8e85493303284b
buenodeandrade <felipebuenodeandrade@gmail.com>
2024-01-05 18:55:01 -0300

    Busquei implantar um sistema que cada resposta gerasse pontos para dois resultados diferentes. Fazendo com que questões de 4 alternativas pudessem gerar 8 resultados diferentes.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -2,29 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
-  selector: 'app-quizz',
-  templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+ selector: 'app-quizz',
+ templateUrl: './quizz.component.html',
+ styleUrls: ['./quizz.component.css']
 })
 
 export class QuizzComponent implements OnInit {
 
-  title:string = ""
+ title:string = ""
 
-  questions:any
-  questionSelected:any
+ questions:any
+ questionSelected:any
 
-  answers:string[] = []
-  answerSelected:string =""
+ answers:string[] = []
+ answerSelected:string =""
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+ questionIndex:number =0
+ questionMaxIndex:number=0
 
-  finished:boolean = false
+ finished:boolean = false
 
-  constructor() { }
+ constructor() { }
 
-  ngOnInit(): void {
+ ngOnInit(): void {
     if(quizz_questions){
       this.finished = false
       this.title = quizz_questions.title
@@ -39,16 +39,16 @@ export class QuizzComponent implements OnInit {
       console.log(this.questionMaxIndex)
     }
 
-  }
+ }
 
-  playerChoose(value:string){
+ playerChoose(value:string){
     this.answers.push(value)
     this.nextStep()
 
-  }
+ }
 
-  async nextStep(){
-    this.questionIndex+=1
+ async nextStep(){
+    this.questionIndex+=2
 
     if(this.questionMaxIndex > this.questionIndex){
         this.questionSelected = this.questions[this.questionIndex]
@@ -57,22 +57,27 @@ export class QuizzComponent implements OnInit {
       this.finished = true
       this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
     }
+ }
+
+ async checkResult(anwsers:string[]){
+  let results: { [key: string]: number } = {};
+  for(let i=0; i<anwsers.length; i++){
+      if(results[anwsers[i]]){
+          results[anwsers[i]] += 1;
+      } else {
+          results[anwsers[i]] = 1;
+      }
   }
 
-  async checkResult(anwsers:string[]){
+  const sortedKeys = Object.keys(results).sort((a, b) => results[b] - results[a]);
+  const highestCount = results[sortedKeys[0]];
+  const mostFrequent = sortedKeys.filter(key => results[key] === highestCount);
 
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
-
-    return result
+  if(mostFrequent.length === 1){
+      return mostFrequent[0];
+  } else {
+      return 'Draw between ' + mostFrequent.join(' and ');
   }
+}
 
 }
